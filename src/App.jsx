4cc53d5ddafc7dd268636086f9fdc1ccde900869
sujalcharati <sujalc024@gmail.com>
2024-11-08 21:22:26 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import './App.css'
 function App() {
 
   return (
-   
-      <div>
-      
+      <div className="font-extrabold text-6xl flex justify-center items-center h-screen animate-pulse">
+        save your penny!
       </div>
-   
   )
 }
 
