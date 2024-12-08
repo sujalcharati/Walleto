5beefada61a2f32a416e49c428487d02272a6b55
sujalcharati <sujalc024@gmail.com>
2024-12-08 19:25:24 +0530

    dotenv conf.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors"
 import User from "./models/user.js"; // Ensure this path is correct and the file exists
+import dotenv from 'dotenv';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect(URL);
 
 // import {connectDB} from './config/db.js';
-import dotenv from 'dotenv';
 // import { signup } from '../controllers/authcontroller.js';
 
 // import authRoutes from "./routes/authroutes.js"
@@ -20,6 +20,8 @@ dotenv.config();
 
 // app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
+const MONGODB_URL = process.env.URL || 'your-default-mongodb-url';
+mongoose.connect(MONGODB_URL, { useUnifiedTopology: true });
 
 
 app.post('/signup', async (req, res) => {
@@ -30,6 +32,7 @@ app.post('/signup', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 
 
