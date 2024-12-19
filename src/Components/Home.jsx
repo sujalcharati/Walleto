d2019422b406d889f10b66a6a4f8f3cd5ade114d
sujalcharati <sujalc024@gmail.com>
2024-12-19 23:08:37 +0530

    new comp

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Alltransaction from "./Alltransaction";
+import Addtransaction from "./Addtransaction";
 export const Home = ()=>{
     return(
         <div>
-            
+            <Alltransaction/>
+            <Addtransaction/>
         </div>
     )
 }
