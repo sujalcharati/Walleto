77d3ce8c47b1f0d1e83e20dbbc2c218f9a13540d
sujalcharati <sujalc024@gmail.com>
2024-12-26 23:41:41 +0530

    header

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Alltransaction from "./Alltransaction";
 import Addtransaction from "./Addtransaction";
+import { Header } from "./Header";
 export const Home = ()=>{
     return(
         <div>
-            hi there
+            <Header/>
             <Alltransaction/>
             <Addtransaction/>
         </div>
