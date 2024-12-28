0288650b9e57630af8e989ffdcf1e7b64f6d73f1
sujalcharati <sujalc024@gmail.com>
2024-12-28 13:55:28 +0530

    three js

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,51 +4,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Sphere } from '@react-three/drei';
 import  { useState } from "react";
 
-// export const Header= ()=>{
-//         const [isOpen, setIsOpen] = useState(false);
-    
-//         const toggleProfile = () => {
-//             setIsOpen(!isOpen);
-//         };
-// return (
-// <div>
-//     <motion.nav
-//       initial={{ y: -50 }}
-//       animate={{ y: 0 }}
-//       transition={{ type: "spring", stiffness: 70 }}
-//       className="bg-gradient-to-r from-blue-600 to-green-500 fixed w-full z-10 shadow-md"
-//     >
-//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-//         <div className="flex justify-between items-center h-16">
-//           <div className="hidden md:flex space-x-4">
-//             <a href="#" className="text-white hover:text-gray-200"> Walleto</a>
-//             <a href="#" className="text-white hover:text-gray-200">Home</a>
-//             <a href="#" className="text-white hover:text-gray-200">Transactions</a>
-//           </div>
-//           <div className="relative flex items-center space-x-4 cursor-pointer" onClick={toggleProfile}>
-//             <img
-//               alt="Profile"
-//               src="https://www.w3schools.com/howto/img_avatar.png"
-//               className="w-10 h-10 rounded-full"
-//             />
-//             {isOpen && (
-//               <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20">
-//                 <div className="px-4 py-2 text-gray-800">
-//                   <p className="font-medium">John Doe</p>
-//                   <p className="text-sm">johndoe@example.com</p>
-//                 </div>
-//                 <div className="border-t border-gray-200"></div>
-//                 <div className="px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">
-//                   Logout
-//                 </div>
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </motion.nav>
-//             <button>add expense</button>
-//             <button>add income</button>
+
 
 
 const AnimatedSphere = () => {
@@ -58,6 +14,9 @@ const AnimatedSphere = () => {
     </Sphere>
   );
 };
+
+
+
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -127,7 +86,6 @@ export const Header = () => {
           </Canvas>
         </div>
       </div>
-    // </div>
   );
 };
 
