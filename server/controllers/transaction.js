a6659e78cd991711738005f7cc73517f4a55d564
sujalcharati <sujalc024@gmail.com>
2025-01-06 16:05:42 +0530

    transaction save

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -2,9 +2,25 @@ import Transaction from "../models/transaction";
 
 const transaction = async (req ,res)=>{
     try {
-        const transaction = new Transaction(req.body);
-        await transaction.save();
-        res.status(201).send(transaction);
+        // const transaction = new Transaction(req.body);
+        const { amount, description ,type ,date }= req.body;
+       
+        const userId = req.user._id; // Assuming user ID is available in req.user
+        const transactionData = await Transaction.create({
+            amount,
+            description,
+            type,
+            date,
+            user: userId
+        });
+
+        console.log(transactionData);
+         const token = jwt.sign(
+            { id: user.id },
+            process.env.secret_key
+         )
+        // const token = req.headers.authorization.split(' ')[1];
+        res.status(201).send(transactionData , token);
       } catch (error) {
         res.status(400).send(error);
       }
