3ae7b1f166196cd064613b3369eed176ad715410
sujalcharati <sujalc024@gmail.com>
2025-01-07 19:26:21 +0530

    middleware_auth

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const authenticateUser = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ error: "No token provided." });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = { _id: decoded.id }; // Add user data to req
+    next();
+  } catch (error) {
+    console.error("Authentication error:", error);
+    res.status(403).json({ error: "Unauthorized access." });
+  }
+};
